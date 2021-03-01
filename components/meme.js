c601002d4a2e702f3b7c5af49a3c367dd1a36d16
Ritik <ritikpr307@gmail.com>
2021-03-01 14:10:37 +0530

    added meme api

---
+++ components/meme.js
@@ -0,0 +1,10 @@
+const axios = require("axios");
+const meme = async (callback) => {
+    await axios.get(`https://api.imgflip.com/get_memes`)
+        .then((res) => {
+            // console.log(res.data.data.memes);
+            callback(res.data.data.memes);
+        }
+        )
+}
+module.exports = meme;
