2f4c97c7364b1d9a1231b9bcd9b90017774c6101
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-03-02 14:26:10 +0530

    Resolved merge conflict

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const entertainment = require("./components/entertainment");
 const science = require("./components/science");
 const meme = require("./components/meme");
-const technology = require("./components/technology");
 
+
+const technology = require("./components/technology");
 //const KEY="3c024bb258b14a3796b0ddd518d59c0f";
 //sortBy-> relevancy, popularity
 let sortBy = "popularity";
@@ -13,7 +14,6 @@ const callback = (data) => {
 }
 entertainment(callback, sortBy);
 science(callback, sortBy);
-technology(callback, sortBy);
 meme(callback);
-
+technology(callback, sortBy);
 
