1906e83af2bd38dc3dd0b3a713c508981fe2da49
Ritik <ritikpr307@gmail.com>
2021-03-04 17:13:48 +0530

    extended functionality in weather API

--- components/weather.js
+++ components/weather.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
-const weather = async (callback,key,city) => {
+const weather = async (callback,key,city,stateCode='',mode='',units='',lang='') => {
     // PUT HTTP
-    await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},${stateCode}&mode=${mode}&units=${units}&lang=${lang}&appid=${key}`)
         .then((res) => {
             callback(res.data);
         })
@@ -9,4 +9,53 @@ const weather = async (callback,key,city) => {
             console.log(err);
         })
 }
-module.exports = weather;
+const weatherByCityId = async (callback,key,id,mode='',units='',lang='') => {
+    // PUT HTTP
+    await axios.get(`http://api.openweathermap.org/data/2.5/weather?id=${id}&mode=${mode}&units=${units}&lang=${lang}&appid=${key}`)
+        .then((res) => {
+            callback(res.data);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+}
+//LatLon
+const weatherByLatLon = async (callback,key,lat,lon,cnt='1',mode='',units='',lang='') => {
+    // PUT HTTP
+    await axios.get(`http://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${lon}&cnt=${cnt}&mode=${mode}&units=${units}&lang=${lang}&appid=${key}`)
+        .then((res) => {
+            callback(res.data);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+}
+const weatherByZip = async (callback,key,zip,ccode='',mode='',units='',lang='') => {
+    // PUT http://
+    await axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},${ccode}&mode=${mode}&units=${units}&lang=${lang}&appid=${key}`)
+        .then((res) => {
+            callback(res.data);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+}
+const weatherByRectZone = async (callback,key,bbox,units='',lang='') => {
+    // PUT http://
+    await axios.get(`http://api.openweathermap.org/data/2.5/box/city?bbox=${bbox}&units=${units}&lang=${lang}&appid=${key}`)
+        .then((res) => {
+            callback(res.data);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+}
+
+module.exports = {
+    weather,
+    weatherByLatLon,
+    weatherByZip,
+    weatherByCityId,
+    weatherByRectZone,
+    
+};
