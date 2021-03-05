f01ebb196b4f08e7559b9e582ce3e286e303b911
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-03-05 21:47:05 +0530

    minor changes

--- components/food.js
+++ components/food.js
@@ -1,16 +1,6 @@
 const axios = require("axios");
 
-// let options = {
-//     method: 'GET',
-//     url: 'https://edamam-food-and-grocery-database.p.rapidapi.com/parser',
-//     params: { ingr: 'banana' },
-//     headers: {
-//         'x-rapidapi-key': 'de231c2f97msh37e7695849917bep1d0608jsn3e905ed8414b',
-//         'x-rapidapi-host': 'edamam-food-and-grocery-database.p.rapidapi.com'
-//     }
-// };
-
-const food = async (foodKEY,foodID, foodName) => {
+const food = async (foodKEY, foodID, foodName) => {
     await axios.get(`https://api.edamam.com/api/food-database/parser?app_key=${foodKEY}&app_id=${foodID}&ingr=${foodName}`)
         .then((res) => {
             // callback(res.data.parsed);
--- components/keyword.js
+++ components/keyword.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
-const { country } = require("..");
-const keyword = async (newsKEY,key,country="",lang="") => {
+
+const keyword = async (newsKEY, key, country = "", lang = "") => {
     await axios.get(`http://newsapi.org/v2/everything?q=${key}&country=${country}&language=${lang}&apiKey=${newsKEY}`)
         .then((res) => {
             //callback(res.data);
             return res.data;
         })
-        .catch((err)=>{
+        .catch((err) => {
             return err;
         })
 }
